b881bac9281f2f23451a4c5167ccf0a9a440b8b8
Iulia Dumitru <iuliadmtru@gmail.com>
2023-09-17 05:17:26 +0200

    Add command for opening chatgpt website

--- vscode-extension/extension.js
+++ vscode-extension/extension.js
@@ -4,6 +4,37 @@ const LogiClient = require('logi-plugin-sdk');
 const fs = require('fs-extra');
 const os = require('os');
 
+// mouse
+const FORWARD_BUTTON = "mx-master-3s-2b034_c86";
+const BACK_BUTTON = "mx-master-3s-2b034_c83";
+const MIDDLE_BUTTON = "mx-master-3s-2b034_c82";
+const THUMB_BUTTON = "mx-master-3s-2b034_c195";
+const TOP_BUTTON = "mx-master-3s-2b034_c196";
+// keyboard
+const F1_BUTTON = "mx-mechanical-2b366_c199"
+const F2_BUTTON = "mx-mechanical-2b366_c200"
+const F3_BUTTON = "mx-mechanical-2b366_c226"
+const F4_BUTTON = "mx-mechanical-2b366_c227"
+const F5_BUTTON = "mx-mechanical-2b366_c259"
+const F6_BUTTON = "mx-mechanical-2b366_c264"
+const F7_BUTTON = "mx-mechanical-2b366_c266"
+const F8_BUTTON = "mx-mechanical-2b366_c284"
+const F9_BUTTON = "mx-mechanical-2b366_c228"
+const F10_BUTTON = "mx-mechanical-2b366_c229"
+const F11_BUTTON = "mx-mechanical-2b366_c230"
+const F12_BUTTON = "mx-mechanical-2b366_c231"
+const VOLUME_DOWN_BUTTON = "mx-mechanical-2b366_c232"
+const VOLUME_UP_BUTTON = "mx-mechanical-2b366_c233"
+const INSERT_BUTTON = "mx-mechanical-2b366_c278"
+const HOME_BUTTON = "mx-mechanical-2b366_c280"
+const END_BUTTON = "mx-mechanical-2b366_c281"
+const PAGE_DOWN_BUTTON = "mx-mechanical-2b366_c283"
+const PAGE_UP_BUTTON = "mx-mechanical-2b366_c282"
+const NUMLOCK_BUTTON = "mx-mechanical-2b366_c10"
+const PROJECT_BUTTON = "mx-mechanical-2b366_c110"
+const SEARCH_BUTTON = "mx-mechanical-2b366_c212"
+const LOCK_BUTTON = "mx-mechanical-2b366_c111"
+
 const eventIdMap = {
 	'mx-master-3s-2b034_c86': 'MX\u0020Master\u00203\u0008S.forwardButton',
 	'mx-master-3s-2b034_c83': 'MX\u0020Master\u00203\u0008S.backButton',
@@ -27,18 +58,18 @@ const eventIdMap = {
 	'mx-mechanical-2b366_c233': 'MX\u0020Mechanical.volumeUp',
 	'mx-mechanical-2b366_c278': 'MX\u0020Mechanical.insert',
 	'mx-mechanical-2b366_c280': 'MX\u0020Mechanical.home',
-	'mx-mechanical-2b366_c282': 'MX\u0020Mechanical.pageUp',
 	'mx-mechanical-2b366_c281': 'MX\u0020Mechanical.end',
 	'mx-mechanical-2b366_c283': 'MX\u0020Mechanical.pageDown',
+	'mx-mechanical-2b366_c282': 'MX\u0020Mechanical.pageUp',
 	'mx-mechanical-2b366_c10': 'MX\u0020Mechanical.numLock',
 	'mx-mechanical-2b366_c110': 'MX\u0020Mechanical.project',
 	'mx-mechanical-2b366_c212': 'MX\u0020Mechanical.search',
 	'mx-mechanical-2b366_c111': 'MX\u0020Mechanical.lock',
 }
 
-// ------------------
+// -----------------------
 // Utils
-// ------------------
+// -----------------------
 
 function eventIdToConfigurationProperty(eventId) {
     return eventIdMap[eventId];
@@ -56,9 +87,9 @@ function eventIdToAction(eventId) {
     configurationPropertyToAction(configurationProperty);
 }
 
-// ------------------
+// -----------------------
 // Action handlers
-// ------------------
+// -----------------------
 
 function iterateThroughIdentifiersForward() {
     const editor = vscode.window.activeTextEditor;
@@ -84,6 +115,54 @@ function goToDefinition() {
     vscode.commands.executeCommand('editor.action.goToDeclaration', wordRange);
 }
 
+class GptCaller {
+    openai;
+    messages = [];
+
+    constructor(apiKey) {
+        const configuration = new Configuration({
+            apiKey: apiKey,
+        });
+        const openai = new OpenAIApi(configuration);
+        this.openai = openai;
+    }
+
+    async askChatGPT(requestText) {
+        this.messages.push({
+            role: "user",
+            content: requestText
+        });
+
+        if (this.openai === null || this.openai === undefined) {
+            console.log("No openai");
+            return "";
+        }
+
+        const completion = await this.openai.createChatCompletion({
+            model: "gpt-3.5-turbo",
+            messages: this.messages
+        });
+        const res = completion.data.choices[0].message;
+        this.messages.push(res);
+
+        return res.content;
+    }
+}
+
+function askGPT() {
+    let apiKey = vscode.workspace.getConfiguration('Logitech').get("Logitech.MX\u0020Mechanical.search.chatGPT");
+    if (apiKey === "") {
+        console.log("You need to provide an API Key");
+        return;
+    }
+
+    vscode.env.openExternal("https://chat.openai.com");
+}
+
+// -----------------------
+// Activate (run extension)
+// -----------------------
+
 function activate(context) {
     const isInitialInstall = context.globalState.get("isInitialInstall", true);
 
@@ -105,13 +184,7 @@ function activate(context) {
     const clientApp = new LogiClient(initialData);
     clientApp.init();
 
-    const FIRST_BUTTON = "mx-master-3s-2b034_c86";
-    const SECOND_BUTTON = "mx-master-3s-2b034_c83";
-    const SCROLL_BUTTON = "mx-master-3s-2b034_c82";
-    const THUMB_BUTTON = "mx-master-3s-2b034_c195";
-    const PROFILE_BUTTON = "mx-master-3s-2b034_c196";
-
-    // eventIdToAction(FIRST_BUTTON);
+    // eventIdToAction(SEARCH_BUTTON);
 
     clientApp.onTriggerAction = async (event) => {
         console.log('onTriggerAction', event);
